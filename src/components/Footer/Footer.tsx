975567065744b202e29c5885f616b4a00a81b191
Lang-Cheng <lang_cl@outlook.com>
2020-12-15 19:08:54 +0800

    Add style for amap popup

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,41 +16,65 @@ const InsetFooter = getInsetFooter(styled);
 
 const MapContainer = () => {
     const [index, setIndex] = useState(0);
+    const [visible, setVisible] = useState(true);
     const data = [
         {
             position: new AMap.LngLat(120.626528, 31.323818, false),
-            title: '<br><h1>苏州市飞天殡葬用品服务有限公司(齐门路)</h1></br>',
-            content: '<br>地址：苏州市姑苏区齐门路87号</br>'
+            title: '<h1 style="font-size: 14px;">苏州市飞天殡葬用品服务有限公司(齐门路)</h1>',
+            content: '<body1 style="font-size: 14px;">地址：苏州市姑苏区齐门路87号</body1>'
         },
         {
             position: new AMap.LngLat(120.643575, 31.320755, false),
-            title: '<br><h1>苏州市飞天殡葬用品服务有限公司(日规路)</h1></br>',
-            content: '<br>地址：苏州市姑苏区日规路6号</br>'
+            title: '<h1 style="font-size: 14px;">苏州市飞天殡葬用品服务有限公司(日规路)</h1>',
+            content: '<body1 style="font-size: 14px;">地址：苏州市姑苏区日规路6号</body1>'
         },
         {
             position: new AMap.LngLat(120.625392, 31.333198, false),
-            title: '<br><h1>苏州市飞天殡葬用品服务有限公司(齐门外大街)</h1></br>',
-            content: '<br>地址：苏州市姑苏区齐门外大街129号</br>'
+            title: '<h1 style="font-size: 14px;">苏州市飞天殡葬用品服务有限公司(齐门外大街)</h1>',
+            content: '<body1 style="font-size: 14px;">地址：苏州市姑苏区齐门外大街129号</body1>'
         }
     ];
 
     return (
         <>
-            <ButtonGroup color="primary" aria-label="outlined primary button group">
-                <Button onClick={() => setIndex(0)}>显示总店（齐门路87号）</Button>
-                <Button onClick={() => setIndex(1)}>显示分店（日规路6号）</Button>
-                <Button onClick={() => setIndex(2)}>显示分店（齐门外大街129号）</Button>
+            <ButtonGroup color="default">
+                <Button
+                    onClick={() => {
+                        setIndex(0);
+                        setVisible(true);
+                    }}
+                >
+                    显示总店
+                </Button>
+                <Button
+                    onClick={() => {
+                        setIndex(1);
+                        setVisible(true);
+                    }}
+                >
+                    显示分店1
+                </Button>
+                <Button
+                    onClick={() => {
+                        setIndex(2);
+                        setVisible(true);
+                    }}
+                >
+                    显示分店2
+                </Button>
             </ButtonGroup>
-            <div style={{ width: '100%', height: '500px' }}>
-                <Map zoom={16}>
+            <div style={{ width: '100%', height: '400px' }}>
+                <Map zoom={16} center={data[index].position}>
                     <ToolBarControl offset={[20, 20]} position="LT" />
-                    <ControlBarControl offset={[20, 20]} position="RT" />
+                    <ControlBarControl offset={[5, 5]} position="RT" />
                     <Marker visiable position={data[0].position} />
                     <Marker visiable position={data[1].position} />
                     <Marker visiable position={data[2].position} />
                     <InfoWindow
-                        visiable
+                        visiable={visible}
+                        closeWhenClickMap
                         position={data[index].position}
+                        onClose={() => setVisible(false)}
                         content={data[index].title + data[index].content}
                     />
                 </Map>
